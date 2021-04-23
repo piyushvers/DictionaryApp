8b021faf44c731b1af1f03867cf80b591e8590f8
piyushvers <piyushversatile96@gmail.com>
2021-04-23 18:49:11 -0500

    Meta info for project

---
+++ DictionaryApp/src/module-info.java
@@ -0,0 +1,7 @@
+module dictionaryApp {
+	exports com.spreetail.assignement.constants;
+	exports com.spreetail.assignement.controller;
+	exports com.spreetail.assignement.exception;
+	exports com.spreetail.assignement.model;
+	exports com.spreetail.assignement.service;
+}
