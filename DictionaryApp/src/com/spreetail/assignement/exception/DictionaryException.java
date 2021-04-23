18c16de1c7e995dcd0ef84f32feb5981197fab23
piyushvers <piyushversatile96@gmail.com>
2021-04-23 18:54:07 -0500

    Custom exception class for the application

---
+++ DictionaryApp/src/com/spreetail/assignement/exception/DictionaryException.java
@@ -0,0 +1,15 @@
+package com.spreetail.assignement.exception;
+
+/**
+ * Exception class for Dictionary App
+ * @author piyus
+ *
+ */
+public class DictionaryException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public DictionaryException(String message){
+		super(message);
+	}
+}
